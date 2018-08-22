2fa22912f22488ef62b67d9f71e1fd508b07ac86
James510 <jamesl510@yahoo.com>
2018-08-22 18:27:18 -0500

    Test Scene Setup

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public int team = 1;
+    Unit selectedUnit;
+
+	void Start ()
+    {
+		
+	}
+
+    void Update()
+    {
+
+    }
+
+    void OnClickCheck()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Unit.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    public int health;
+    public int actionPoints;
+    public int attack;
+}
