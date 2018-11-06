25d60a2d949230f1a719f250c9e1c7e2ac3ca636
James510 <jamesl510@yahoo.com>
2018-11-06 07:57:11 -0600

    Wave Events

--- Assets/Scripts/ScenarioZeroA.cs
+++ Assets/Scripts/ScenarioZeroA.cs
@@ -3,19 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using MEC;
 
 public class ScenarioZeroA : MonoBehaviour
 {
     PlayerController pc;
-    GameObject resultScreen;
-
+    GameObject resultScreen,enemySpawnPoint;
+    public GameObject enemyACMech, enemyShotgunMech;
     void Awake()
     {
         pc = FindObjectOfType<PlayerController>();
+        enemySpawnPoint = GameObject.Find("EnemySpawnPoint");
     }
 
 	void Start ()
     {
-		
+        Timing.RunCoroutine(WaveEvents());
 	}
+
+    IEnumerator<float> WaveEvents()
+    {
+        while(pc.turnCount != 2)
+        {
+            yield return 0;
+        }
+
+        while(pc.turnCount != 6)
+        {
+            yield return 0;
+        }
+    }
 }
